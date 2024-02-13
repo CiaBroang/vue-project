d40fa020d78a57461ecd2123ca907e6d5dfe297b
CiaBroang <cbroang@gmail.com>
2024-02-13 18:43:41 +0100

    New aligned buy-button on the same row. Product card and grid styling. New stepper for the cart

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1> This is the cart </h1>
+    <h1> Varukorg </h1>
+
+    <v-stepper :items="['Granska', 'Leverans', 'Betalning']">
+  <template v-slot:item.1>
+    <v-card title="Step One" flat>...</v-card>
+  </template>
+
+  <template v-slot:item.2>
+    <v-card title="Step Two" flat>...</v-card>
+  </template>
+
+  <template v-slot:item.3>
+    <v-card title="Step Three" flat>...</v-card>
+  </template>
+</v-stepper>
+    
+
+    
 </template>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,42 +4,46 @@ import axios from 'axios';
 export default {
   data() {
     return {
-    products: []
-  };
-},
-methods: {
-  fetchData() {
-    axios.get('https://fakestoreapi.com/products')
-    .then(response => {
-      this.products = response.data;
-    })
-    .catch(error => {
-      console.log('error fetching data:', error);
-    });
-  }
-},
-mounted() {
-  this.fetchData();
+      products: []
+    };
+  },
+  methods: {
+    fetchData() {
+      axios.get('https://fakestoreapi.com/products')
+        .then(response => {
+          this.products = response.data;
+        })
+        .catch(error => {
+          console.log('error fetching data:', error);
+        });
+    }
+  },
+  mounted() {
+    this.fetchData();
   }
 };
 </script>
 
 
 <template>
-      <v-container>
-        <v-row>
-          <v-col
-            v-for="product in products"
-            :key="product.id"
-            cols="4"
-          >
-            <v-card height="200">
-              <v-img :src="product.image" height="100"></v-img>
-              <v-card-title>{{ product.title }}</v-card-title>
-              <p v-if="product.rating.rate >= 4">Populär produkt!</p>
-              <v-card-subtitle>{{ product.price }} kr</v-card-subtitle>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+  <v-container>
+    <v-row>
+      <v-col v-for="product in products" :key="product.id" cols="auto">
+        <v-card height="300" width="200">
+          <v-img :src="product.image" height="100"></v-img>
+          <container style="padding: 20px; display: flex; flex-direction: column;">
+            <v-card-title style="font-size: 18px;">{{ product.title }}</v-card-title>
+            <v-card-subtitle v-if="product.rating.rate >= 4">Populär produkt!</v-card-subtitle>
+            <!-- "non-breaking space" -->
+            <v-card-subtitle v-else style="line-height: 1.2em;">&nbsp;</v-card-subtitle>
+            <div style="margin-top: auto;">
+              <v-card-subtitle style="font-weight: bold; color: #212121;">{{ product.price }} EUR</v-card-subtitle>
+              <v-btn size="small" color="indigo-darken-3" style="margin: 1.2em;">KÖP</v-btn>
+            </div>
+          </container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-pagination :length="4"></v-pagination>
 </template>
